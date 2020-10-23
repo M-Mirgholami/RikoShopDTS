ee557c8542be3a3e3cea4bc150f484b2544eb971
MohadmadReza <mr.rahmatzade371@gmail.com>
2020-10-24 00:19:59 +0330

    first test

---
+++ RikoShopDTS/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RikoShopDTS
+{
+    public static class Extensions
+    {
+        public static string UnicodeToString(this string unicodedstr)
+        {
+            string unicodestring = unicodedstr;
+
+            // Create two different encodings.
+            Encoding ascii = Encoding.ASCII;
+            Encoding unicode = Encoding.Unicode;
+            //Encoding Utf8 = Encoding.UTF8;
+
+            // // Convert the string into a byte array.
+            byte[] unicodeBytes = unicode.GetBytes(unicodestring);
+
+            // // Perform the conversion from one encoding to the other.
+            byte[] ascibytes = Encoding.Convert(unicode, ascii, unicodeBytes);
+
+            // // Convert the new byte[] into a char[] and then into a string.
+            char[] asciiChars = new char[ascii.GetCharCount(ascibytes, 0, ascibytes.Length)];
+            ascii.GetChars(ascibytes, 0, ascibytes.Length, asciiChars, 0);
+            string asciiString = new string(asciiChars);
+
+            // // Display the strings created before and after the conversion.
+            //MessageBox.Show("Original string is"+unicodeString);
+            return asciiString;
+        }
+    }
+}
--- RikoShopDTS/Program.cs
+++ RikoShopDTS/Program.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using WooCommerceNET;
+using WooCommerceNET.WooCommerce.v3;
 
 namespace RikoShopDTS
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
+            try
+            {
+                //dapper test
+                var db = new SqlConnection(secrets.ConnectionString);
+                db.Open();
+                var ddd = db.Query<object>("select * from users");
 
+
+                //woocommerce api test
+                RestAPI rest = new RestAPI(secrets.SiteAddress, secrets.ConsumerKey, secrets.ConsumerPass);
+                WCObject wc = new WCObject(rest);
+
+                //Get all products
+                var products = await wc.Product.GetAll();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message.UnicodeToString();
+                return 0;
+            }
         }
     }
 }
